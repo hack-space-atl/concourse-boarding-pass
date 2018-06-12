4ebb9e46340a7f5418de501a394bc9c0fde6661d
Karthik Rao <karthik.rao543@gmail.com>
2018-06-12 19:08:09 -0400

    added react-router-dom

--- src/index.js
+++ src/index.js
@@ -6,5 +6,9 @@ import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter } from 'react-router-dom';
 
 
-ReactDOM.render(<BrowserRouter> <App /> </BrowserRouter>, document.getElementById('root'));
+ReactDOM.render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+    , document.getElementById('root'));
 registerServiceWorker();
