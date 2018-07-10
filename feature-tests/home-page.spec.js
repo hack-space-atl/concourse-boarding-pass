79ae52cff8edded69c851a710e70c125828db688
Marquita Priester and Yosep Kim <ykim@MacBook-Pro.local>
2018-07-10 19:08:59 -0400

    Adds some more e2e tests

--- feature-tests/home-page.spec.js
+++ feature-tests/home-page.spec.js
@@ -1,11 +1,33 @@
-describe('Concourse App', function() {
-    it('displays home page', async () => {
+describe('Concourse App', function () {
+    beforeEach(() => {
         browser.ignoreSynchronization = true;
 
         browser.get('/');
+    });
 
+    it('displays home page', async () => {
         const title = element(by.css('h1.App-title'));
 
         expect(title.getText()).toEqual('Concourse Boarding Pass');
+    });
+
+    it('redirects users to the set up page', async () => {
+        element(by.css('button:first-child')).click();
+
+        expect(element(by.css('h2')).getText()).toEqual('Configuration');
+    });
+
+    it('redirects users to the installation page', async () => {
+        element(by.css('button:nth-child(2)')).click();
+
+        expect(element(by.css('.install')).getText()).toContain('Need help installing Concourse?');
+    });
+
+    it('redirects users to the set up page from the installation page', async () => {
+        element(by.css('button:nth-child(2)')).click();
+
+        element(by.css('.installed-concourse')).click();
+
+        expect(element(by.css('h2')).getText()).toEqual('Configuration');
     });
 });
